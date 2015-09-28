46eaab2a88201324fd2d3db5a596caea4999ddc6
korshuk <korshuk@lyceum.by>
2015-09-28 21:14:48 +0300

    first

---
+++ code/1/js/window-object.js
@@ -0,0 +1,9 @@
+var msg = '<h2>browser window</h2><p>width: ' + window.innerWidth + '</p>';
+msg += '<p>height: ' + window.innerHeight + '</p>';
+msg += '<h2>history</h2><p>items: ' + window.history.length + '</p>';
+msg += '<h2>screen</h2><p>width: ' + window.screen.width + '</p>';
+msg += '<p>height: ' + window.screen.height + '</p>';
+
+var el = document.getElementById('info');
+el.innerHTML = msg;
+alert('Current page: ' + window.location);
