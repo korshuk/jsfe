95af6d05dc41c96f70e6303fc578282ea0e5e4fa
korshuk <korshuk@lyceum.by>
2015-10-27 19:59:05 +0300

    '27.10.2015'

---
+++ code/8/js/accordion.js
@@ -0,0 +1,9 @@
+$('.accordion').on('click', '.accordion-control', function(e){
+  
+  e.preventDefault();
+  
+  $(this)
+    .next('.accordion-panel')
+    .not(':animated')
+    .slideToggle();
+});
